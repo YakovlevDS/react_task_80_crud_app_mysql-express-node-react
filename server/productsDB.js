fe66b60f4e1902b96af956c4118d40c15c0fe074
Dmitriy Yakovlev <1d3ddd@gmail.com>
2021-12-16 20:55:59 +0200

    add

---
+++ server/productsDB.js
@@ -0,0 +1,18 @@
+products = [
+  {
+    id: 1,
+    title: "orange",
+    price: 200,
+  },
+  {
+    id: 2,
+    title: "greyfruit",
+    price: 300,
+  },
+  {
+    id: 3,
+    title: "apple",
+    price: 100,
+  }
+];
+
---
+++ server/server.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const mysql = require('mysql');
+const app = express();
+const port = 5000;
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+var mysql = require('mysql');
+
+var con = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "root"
+});
+
+con.connect(function(err) {
+  if (err) throw err;
+  console.log("Connected!");
+});
+
+app.listen(port, () => {
+  console.log(`Server starting on http://localhost:${port}`);
+});
